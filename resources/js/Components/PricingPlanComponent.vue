76988d4195969a0fd9226cfc3e000bd8d24f1073
bigtank <codeplay.erniie@gmail.com>
2023-08-12 03:56:44 +0100

    added logic to pricing plan component

--- resources/js/Components/PricingPlanComponent.vue
+++ resources/js/Components/PricingPlanComponent.vue
@@ -7,86 +7,38 @@
             </div>
 
             <div class="pricing-tab">
-                <button class="btn active">bill anually</button>
-                <button class="btn">bill monthly</button>
+                <button class="btn" :class="{ active: isMonthlyBilling }" @click="isMonthlyBilling = true">bill monthly</button>
+                <button class="btn" :class="{ active: !isMonthlyBilling }" @click="isMonthlyBilling = false">bill anually</button>
             </div>
 
             <div class="pricing-plans">
-                <div class="plan">
-                    <div class="content-wrap">
-                        <span class="plan__title">silver card</span>
-                        <div class="price">
-                            <sup>$</sup>
-                            <span>9.99</span>
+                <div v-for="(plan, index) in pricingPlans" :key="index" class="plan" :class="{ discounted: !isMonthlyBilling  && plan.discount }">
+                    <div class="plan-card">
+                        <div class="card">
+                            <span v-if="!isMonthlyBilling && plan.discount" class="discount-label">save {{ plan.discount }}%</span>
+                            <span class="plan__title">{{ plan.title }} card</span>
+                            <div class="price">
+                                <sup>$</sup>
+                                <span v-if="isMonthlyBilling">{{ plan.monthlyPrice }}</span>
+                                <span v-else>{{ plan.annualPrice }}</span>
+                            </div>
+                            <span v-if="isMonthlyBilling" class="price-label">usd per month</span>
+                            <span v-else class="price-label">usd per year</span>
+                            <div class="credit-card">
+                                <img src="https://www.transparentpng.com/thumb/credit-card/8p4jX1-blank-credit-card-pic.png" alt="Blank Credit Card Pic @transparentpng.com">
+                            </div>
+                            <span class="desc">{{ plan.desc }}</span>
+                            <hr>
+                            <ul class="benefits">
+                                <li v-for="(item, index) in plan.benefits" :key="index" class="item">
+                                    <ion-icon name="checkmark-circle"></ion-icon>
+                                    <span>{{ item }}</span>
+                                </li>
+                            </ul>
+                            <ButtonComponent type="outline">get started</ButtonComponent>
                         </div>
-                        <span class="price-label">user per month</span>
-                        <div class="credit-card">
-                            <img src="https://www.transparentpng.com/thumb/credit-card/8p4jX1-blank-credit-card-pic.png" alt="Blank Credit Card Pic @transparentpng.com">
-                        </div>
-                        <span class="desc">Start your business with a simple account to manage it.</span>
-                        <hr>
-                        <ul class="benefits">
-                            <li class="item">
-                                <ion-icon name="checkmark-circle"></ion-icon>
-                                <span>Free payment to other Pay accounts</span>
-                            </li>
-                            <li class="item">
-                                <ion-icon name="checkmark-circle"></ion-icon>
-                                <span>25 free local transfers</span>
-                            </li>
-                            <li class="item">
-                                <ion-icon name="checkmark-circle"></ion-icon>
-                                <span>Free ATM withdrawals in USD</span>
-                            </li>
-                            <li class="item">
-                                <ion-icon name="checkmark-circle"></ion-icon>
-                                <span>5 free international transfers fee</span>
-                            </li>
-                        </ul>
-                        <ButtonComponent type="outline">get started</ButtonComponent>
                     </div>
-                </div>
-                <div class="plan">
-                    <div class="content-wrap">
-                        <span class="plan__title">platinum card</span>
-                        <div class="price">
-                            <sup>$</sup>
-                            <span>9.99</span>
-                        </div>
-                        <span class="price-label">user per month</span>
-                        <div class="credit-card">
-                            <img src="https://www.transparentpng.com/thumb/credit-card/8p4jX1-blank-credit-card-pic.png" alt="Blank Credit Card Pic @transparentpng.com">
-                        </div>
-                        <span class="desc">Available for businesses with large payments business models.</span>
-                        <hr>
-                        <ul class="benefits">
-                            <li class="item">
-                                <ion-icon name="checkmark-circle"></ion-icon>
-                                <span>Free payment to other Pay accounts</span>
-                            </li>
-                            <li class="item">
-                                <ion-icon name="checkmark-circle"></ion-icon>
-                                <span>Unlimited free local transfers</span>
-                            </li>
-                            <li class="item">
-                                <ion-icon name="checkmark-circle"></ion-icon>
-                                <span>Free ATM withdrawals in USD & Euros</span>
-                            </li>
-                            <li class="item">
-                                <ion-icon name="checkmark-circle"></ion-icon>
-                                <span>Unlimited International transfers fee</span>
-                            </li>
-                            <li class="item">
-                                <ion-icon name="checkmark-circle"></ion-icon>
-                                <span>Priority 24/7 support</span>
-                            </li>
-                            <li class="item">
-                                <ion-icon name="checkmark-circle"></ion-icon>
-                                <span>Virtual card payment</span>
-                            </li>
-                        </ul>
-                        <ButtonComponent type="outline">get started</ButtonComponent>
-                    </div>
+                    <span v-if="!isMonthlyBilling && plan.discount" class="discount-message">*Limited offer, save {{ plan.discount }}% for annual plan {{ plan.title }} card.</span>
                 </div>
             </div>
         </div>
@@ -95,6 +47,39 @@
 
 <script setup>
 import ButtonComponent from './ButtonComponent.vue';
+import { ref } from 'vue';
+
+let isMonthlyBilling = ref(true);
+
+const pricingPlans = [
+    {
+        title: 'silver',
+        monthlyPrice: 9.99,
+        annualPrice: 109.99,
+        benefits: [
+            'Free payment to other Pay accounts',
+            '25 free local transfers',
+            'Free ATM withdrawals in USD',
+            '5 free international transfers fee'
+        ],
+        desc: 'Start your business with a simple account to manage it.',
+    },
+    {
+        title: 'platinum',
+        monthlyPrice: 49.99,
+        annualPrice: 549.99,
+        discount: '20',
+        benefits: [
+            'Free payment to other Pay accounts',
+            'Unlimited free local transfers',
+            'Free ATM withdrawals in USD & Euros',
+            'Unlimited International transfers fee',
+            'Priority 24/7 support',
+            'Virtual card payment'
+        ],
+        desc: 'Available for businesses with large payments business models.',
+    }
+];
 
 
 </script>
